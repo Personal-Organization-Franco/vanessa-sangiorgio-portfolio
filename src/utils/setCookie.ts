c19688d609b31c14ec625284deff547872b7f162
Francesco Albanese <albanesefrancesco.af@gmail.com>
2023-11-21 12:58:27 +0000

    feat: check if document is defined

--- src/utils/setCookie.ts
+++ src/utils/setCookie.ts
@@ -3,5 +3,9 @@ export function setCookie(
   value: string = "true",
   maxAge: number = 60 * 60 * 24 * 30,
 ) {
-  return (document.cookie = `${name}=${value}; max-age=${maxAge};`);
+  if (typeof document !== "undefined") {
+    return (document.cookie = `${name}=${value}; max-age=${maxAge};`);
+  }
+
+  return "";
 }
